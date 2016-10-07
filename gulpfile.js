10134a3970367195b7b908598a6fcfe6e2d0ee6f
Hajir Javaheri <javaheri@Hajirs-iMac.local>
2016-10-07 09:25:59 +0330

    Added watch to default

--- gulpfile.js
+++ gulpfile.js
@@ -44,4 +44,4 @@ gulp.task('watch', function() {
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass'])
 });
-gulp.task('default', ['coffee', 'js', 'compass']);
+gulp.task('default', ['coffee', 'js', 'compass', 'watch']);
